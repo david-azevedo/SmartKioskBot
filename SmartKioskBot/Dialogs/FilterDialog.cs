8bc36f51b591926c3fc6167bc0f55caf175a54e7
root <root@DESKTOP-HQA4PR5.localdomain>
2018-04-14 19:33:05 +0100

    small db population and access

---
+++ SmartKioskBot/Dialogs/FilterDialog.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using MongoDB.Driver;
+using SmartKioskBot.Helpers;
+using SmartKioskBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartKioskBot.Dialogs
+{
+    [Serializable]
+    public class FilterDialog : IDialog<object>
+    {
+#pragma warning disable 1998
+        public async Task StartAsync(IDialogContext context)
+#pragma warning restore 1998
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result as Activity;
+
+            var userInput = (message.Text != null ? message.Text : "").Split(new[] { ' ' }, 2);
+
+            string[] details = message.Text.Split(' ');
+
+            // Get products and create a reply to reply back to the user.
+            var brand = details[1];//message.Text; //TODO
+            List<Product> products = GetProductsForUser(brand);
+
+            var reply = context.MakeMessage();
+            reply.Text = BuildProductText(products, brand);
+            //reply.Text = "tou aqui";
+
+            await context.PostAsync(reply);
+
+            context.Done<object>(null);
+        }
+
+        private static List<Product> GetProductsForUser(string brand)
+        {
+            var collection = DbSingleton.GetDatabase().GetCollection<Product>(AppSettings.CollectionName);
+            var filter = Builders<Product>.Filter.Where(x => x.Brand.ToLower() == brand.ToLower());
+
+            /*
+            if (!String.IsNullOrEmpty(searchText))
+            {
+                filter = filter & Builders<Product>.Filter.Where(x => x.Content.Contains(searchText));
+            }*/
+
+            var products = collection.Find(filter).ToList();
+            return products;
+        }
+
+        private static string BuildProductText(List<Product> products, string brand)
+        {
+            var text = "";
+
+            text += "Filter (" + brand + "):\n";
+
+            foreach (var product in products)
+            {
+                if (product != null)
+                {
+                    
+                    text += "\n- " + product.Model;
+                }
+            }
+
+            return text;
+        }
+    }
+}
---
+++ SmartKioskBot/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace SmartKioskBot.Models
+{
+    public class Product
+    {
+        [BsonId(IdGenerator = typeof(CombGuidGenerator))]
+        public Guid Id { get; set; }
+
+        [BsonElement("Brand")]
+        public string Brand { get; set; }
+
+        [BsonElement("Model")]
+        public string Model { get; set; }
+
+        [BsonElement("Category")]
+        public string Category { get; set; }
+
+        [BsonElement("Price")]
+        public Double Price { get; set; }
+
+        [BsonElement("Weight")]
+        public Double Weight { get; set; }
+
+        [BsonElement("Height")]
+        public Double Height { get; set; }
+
+        [BsonElement("Colour")]
+        public string Colour { get; set; }
+    }
+}
