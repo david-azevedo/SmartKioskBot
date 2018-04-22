a1a80065897662f7747ef5730f62087a1817bb82
Unknown <catramos96@gmail.com>
2018-04-22 18:13:38 +0100

    state information about bot is being saved

--- SmartKioskBot/Global.asax.cs
+++ SmartKioskBot/Global.asax.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using Autofac;
+using System.Configuration;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Dialogs.Internals;
 
 namespace SmartKioskBot
 {
@@ -12,6 +18,25 @@ namespace SmartKioskBot
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var uri = new Uri(ConfigurationManager.AppSettings["DocumentDbUrl"]);
+            var key = ConfigurationManager.AppSettings["DocumentDbKey"];
+            var store = new DocumentDbBotDataStore(uri, key);
+
+            Conversation.UpdateContainer(
+                        builder =>
+                        {
+                            builder.Register(c => store)
+                                .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                                .AsSelf()
+                                .SingleInstance();
+
+                            builder.Register(c => new CachingBotDataStore(store, CachingBotDataStoreConsistencyPolicy.ETagBasedConsistency))
+                                .As<IBotDataStore<BotData>>()
+                                .AsSelf()
+                                .InstancePerLifetimeScope();
+
+                        });
         }
     }
 }
