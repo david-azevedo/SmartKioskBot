cdbff551ea6f40298079af8446d5e3fb41c3b755
José Aleixo <josealeixo.pc@gmail.com>
2018-04-26 16:48:20 +0100

    Added ReadMe file. Created comparison for CPU between products.

---
+++ SmartKioskBot/Logic/Comparable.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartKioskBot.Logic
+{
+    public abstract class Comparable
+    {
+        private string compareExceptionMessage = "Product to compare is not of the same class.";
+
+        /// <summary>
+        /// This method is used to compare the current computer part with another part. It compares attributes of the parts directly, so it isn't as truthful as it should be.
+        /// </summary>
+        /// <param name="c">The part to compare to.</param>
+        /// <returns>Returns an integer with the value of the comparison. Less than 0 means the current part is worst. More than 0 means it's better. Equal to 0 means it's a draw.</returns>
+        public abstract int CompareTo(Comparable c);
+
+        public class CPU : Comparable
+        {
+            private int numberOfCores;
+            private float clock; // in GHz
+
+            public CPU (int numberOfCores, float clock)
+            {
+                this.numberOfCores = numberOfCores;
+                this.clock = clock;
+            }
+
+            public override int CompareTo(Comparable c)
+            {
+                if (!(c is CPU))
+                {
+                    throw new Exception(this.compareExceptionMessage);
+                }
+
+                CPU cpu = (CPU)c;
+                int result = 0;
+
+                if(this.numberOfCores > cpu.numberOfCores)
+                {
+                    result++;
+                }
+                else
+                {
+                    result--;
+                }
+
+                if(this.clock > cpu.clock)
+                {
+                    result++;
+                }
+                else
+                {
+                    result--;
+                }
+
+                return result;
+                
+            }
+        }
+    }
+}
---
+++ SmartKioskBot/Logic/Comparator.cs
@@ -0,0 +1,52 @@
+﻿using SmartKioskBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartKioskBot.Logic
+{
+    public class Comparator
+    {
+        public Product GetBestProduct (List<Product> products)
+        {
+            int bestCPUIndex = GetBestCPU(products);
+
+            return products[bestCPUIndex];
+        }
+
+        public int GetBestCPU (List<Product> products)
+        {
+            List<Comparable.CPU> cpus = new List<Comparable.CPU>();
+
+            for(int i = 0; i < products.Count; i++)
+            {
+                Product currentProduct = products[i];
+                cpus.Add(new Comparable.CPU(int.Parse(currentProduct.CoreNr), float.Parse(currentProduct.CPUSpeed)));
+            }
+
+            return GetBestPart(cpus);
+        }
+
+        public int GetBestPart<T> (List<T> comparables) where T : Comparable
+        {
+            int indexOfBestComparable = -1;
+
+            for(int i = 0; i < comparables.Count - 1; i++)
+            {
+                int comparisonResult = comparables[i].CompareTo(comparables[i + 1]);
+
+                if (comparisonResult > 0)
+                {
+                    indexOfBestComparable = i;
+                }
+                else if (comparisonResult < 0)
+                {
+                    indexOfBestComparable = i + 1;
+                }
+            }
+
+            return indexOfBestComparable;
+        }
+    }
+}
