6e07c05dc3c32ad003920716f8838b6f04dfc2a8
José Aleixo <josealeixo.pc@gmail.com>
2018-04-28 19:24:03 +0100

    Added other package to connect to Cosmos.

--- SmartKioskBot/Logic/Comparator.cs
+++ SmartKioskBot/Logic/Comparator.cs
@@ -10,17 +10,28 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
 
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+// ADD THIS PART TO YOUR CODE
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+using System.Security.Authentication;
+
 namespace SmartKioskBot.Logic
 {
     public static class Comparator
     {
-        public static void GetBestProduct (List<Product> products)
+        public static void GetBestProduct(List<Product> products)
         {
             // The key is the index of the product in the list argument. The value is the number of times a component of the product is 
             // better than the others.
             Dictionary<int, int> punctuations = new Dictionary<int, int>();
 
-            for(int i = 0; i < products.Count; i++)
+            for (int i = 0; i < products.Count; i++)
             {
                 punctuations.Add(i, 0); // every product starts with punctuation equal to zero
             }
@@ -36,9 +47,9 @@ namespace SmartKioskBot.Logic
 
             int bestOverallIndex = 0;
 
-            for(int i = 1; i < punctuations.Count; i++)
+            for (int i = 1; i < punctuations.Count; i++)
             {
-                if(punctuations[i] > punctuations[bestOverallIndex])
+                if (punctuations[i] > punctuations[bestOverallIndex])
                 {
                     bestOverallIndex = i;
                 }
@@ -47,7 +58,7 @@ namespace SmartKioskBot.Logic
             // TODO: do something with the indexes, like showing on a card the comparison
         }
 
-        public static int GetBestCPU (List<Product> products)
+        public static int GetBestCPU(List<Product> products)
         {
             List<Comparable.CPU> cpus = new List<Comparable.CPU>();
 
@@ -59,7 +70,7 @@ namespace SmartKioskBot.Logic
                 Product currentProduct = products[i];
                 int numOfCores = 0;
 
-                if(currentProduct.CoreNr != null)
+                if (currentProduct.CoreNr != null)
                 {
                     if (dualRegex.Match(currentProduct.CoreNr).Length != 0) // match dual core (2 cores)
                     {
@@ -76,12 +87,12 @@ namespace SmartKioskBot.Logic
 
                 if (currentProduct.CPUSpeed != null)
                 {
-                   cpuSpeed = float.Parse(currentProduct.CPUSpeed, System.Globalization.CultureInfo.InvariantCulture);
+                    cpuSpeed = float.Parse(currentProduct.CPUSpeed, System.Globalization.CultureInfo.InvariantCulture);
                 }
 
                 string name = null;
 
-                if(currentProduct.CPU != null)
+                if (currentProduct.CPU != null)
                 {
                     name = currentProduct.CPU;
                 }
@@ -96,14 +107,14 @@ namespace SmartKioskBot.Logic
         {
             List<Comparable.RAM> rams = new List<Comparable.RAM>();
 
-            Regex numberRegex = new Regex(@"\d+"); 
+            Regex numberRegex = new Regex(@"\d+");
 
             for (int i = 0; i < products.Count; i++)
             {
                 Product currentProduct = products[i];
                 int memory = 0;
 
-                if(currentProduct.RAM != null)
+                if (currentProduct.RAM != null)
                 {
                     memory = int.Parse(numberRegex.Match(currentProduct.RAM).Value);
                 }
@@ -128,13 +139,13 @@ namespace SmartKioskBot.Logic
                 int resolution_x = 0, resolution_y = 0;
                 float diagonal = 0;
                 bool touch = false;
-                
-                if(currentProduct.ScreenDiagonal != null)
+
+                if (currentProduct.ScreenDiagonal != null)
                 {
                     diagonal = float.Parse(numberRegex.Match(currentProduct.ScreenDiagonal).Value);
                 }
-                
-                if(currentProduct.ScreenResolution != null)
+
+                if (currentProduct.ScreenResolution != null)
                 {
                     Match resMatch = numberRegex.Match(currentProduct.ScreenResolution);
                     resolution_x = int.Parse(resMatch.Value);
@@ -152,18 +163,18 @@ namespace SmartKioskBot.Logic
                         touch = true;
                     }
                 }
-               
+
                 screens.Add(new Comparable.Screen(diagonal, resolution_x, resolution_y, touch));
             }
 
             return GetBestPart(screens);
         }
 
-        public static int GetBestPart<T> (List<T> comparables) where T : Comparable
+        public static int GetBestPart<T>(List<T> comparables) where T : Comparable
         {
-            int indexOfCurrentBest= -1;
+            int indexOfCurrentBest = -1;
 
-            for(int i = 0; i < comparables.Count - 1; i++)
+            for (int i = 0; i < comparables.Count - 1; i++)
             {
                 int comparisonResult = comparables[i].CompareTo(comparables[i + 1]);
 
@@ -182,6 +193,37 @@ namespace SmartKioskBot.Logic
 
         public static void Test()
         {
+            string host = "skb-cosmos-mongo-v1.documents.azure.com";
+
+            MongoClientSettings settings = new MongoClientSettings();
+            settings.Server = new MongoServerAddress(host, 10255);
+            settings.UseSsl = true;
+            settings.SslSettings = new SslSettings();
+            settings.SslSettings.EnabledSslProtocols = SslProtocols.Tls12;
+            
+            string dbName = "db";
+            string collectionName = "Products";
+
+            MongoIdentity identity = new MongoInternalIdentity(dbName, "skb-cosmos-mongo-v1");
+            MongoIdentityEvidence evidence = new PasswordEvidence("mD9P6JY7iA863jozlyaXWjTdkvEMJyG6N3mHFF9iTCYyUfN0dYfrLQXx3Fkg4meW9EVGnwWSMlJGNpg34OepnA==");
+
+            settings.Credential = new MongoCredential("SCRAM-SHA-1", identity, evidence);
+
+            MongoClient client = new MongoClient(settings);
+
+            try
+            {
+                var database = client.GetDatabase(dbName);
+                var productCollection = database.GetCollection<Product>(collectionName);
+
+                List<Product> products = productCollection.Find(new BsonDocument()).ToList();
+                Debug.WriteLine(products.Count);
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine("{0} error occurred: {1}, Message: {2}", e.Message, baseException.Message);
+            }
 
             // var collection = DbSingleton.GetDatabase().GetCollection<Product>(AppSettings.CollectionName);
             // var builder = Builders<Product>.Filter;
@@ -191,6 +233,7 @@ namespace SmartKioskBot.Logic
 
             // var product1 = collection.Find(filter1).FirstOrDefault();
             // var product2 = collection.Find(filter2).FirstOrDefault();
+            
 
             Product product1 = new Product();
             product1.CPU = "Intel Core i7-6500U";
