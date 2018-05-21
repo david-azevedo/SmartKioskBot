feef33488558a77797bbb61754aa3cf4fa3d0cac
Catarina Ramos <catramos96>
2018-05-21 10:18:18 +0100

    fix

--- SmartKioskBot/Dialogs/RootDialog.cs
+++ SmartKioskBot/Dialogs/RootDialog.cs
@@ -87,6 +87,7 @@ namespace SmartKioskBot.Dialogs
         {
             TryIdentification(context);
             FilterIntentScore(context, result);
+
             string message = "Desculpa mas não entendi aquilo que disseste. Podes refrasear por favor? :)";
             await context.PostAsync(message);
 
@@ -118,6 +119,7 @@ namespace SmartKioskBot.Dialogs
         {
             TryIdentification(context);
             FilterIntentScore(context, result);
+
             var reply = WishListDialog.ViewWishList(context, ContextController.GetContext(user.Id));
             await Helpers.BotTranslator.PostTranslated(context, reply, context.MakeMessage().Locale);
             context.Done<object>(null);
@@ -215,6 +217,7 @@ namespace SmartKioskBot.Dialogs
         {
             TryIdentification(context);
             FilterIntentScore(context, result);
+
             var reply = FilterDialog.CleanFilter(context, this.user, ContextController.GetContext(user.Id), result.Entities);
             await Helpers.BotTranslator.PostTranslated(context, reply, context.MakeMessage().Locale);
             context.Done<object>(null);
@@ -243,6 +246,8 @@ namespace SmartKioskBot.Dialogs
         public async Task StockInStore(IDialogContext context, LuisResult result)
         {
             TryIdentification(context);
+	    FilterIntentScore(context, result);
+
             var idx = result.Query.LastIndexOf(":");
             string id = result.Query.Remove(0, idx + 1).Replace(" ", "");
 
@@ -304,180 +309,5 @@ namespace SmartKioskBot.Dialogs
                 Debug.WriteLine(e.ToString());
             }
         }
-
-        /*
-        [LuisIntent("StoreLocation")]
-        public void StoreLocation(IDialogContext context, LuisResult result)
-        {
-            FilterIntentScore(context, result);
-            
-            context.Done<object>(null);
-        }
-
-        private Task Done(IDialogContext context, IAwaitable<object> result)
-        {
-
-            context.Done<object>(null);
-            return null;
-        }*/
-
-        /* [LuisIntent("Recommendation")]
-         public void Recommendation(IDialogContext context, LuisResult result)
-         {
-            FilterIntentScore(context, result);
-            
-            context.Done<object>(null);
-         }*/
-
-        /* [LuisIntent("StoreLocation")]
-         public void StoreLocation(IDialogContext context, LuisResult result)
-         {
-            FilterIntentScore(context, result);
-            
-            context.Done<object>(null);
-         }*/
-
-        /* [LuisIntent("ViewWishList")]
-         public void ViewWishList(IDialogContext context, LuisResult result)
-         {
-            FilterIntentScore(context, result);
-            
-            FilterDialog w = new FilterDialog();
-            IMessageActivity r = w.filtering(result.Entities, context.MakeMessage());
-
-            context.Done<object>(null);
-         }*/
-
-
-        /*
-        var message = await activity as Activity;
-
-
-        // Get the command, or the first word, that the user typed in.
-        var userInput = message.Text != null ? message.Text : "";
-        var command = (userInput.Split(new[] { ' ' }, 3))[0];
-
-
-        reply.Text = "Comandos:\n\n" +
-                "filter [marca/preço/nome] [operator] [valor] \n\n" +
-                "filter-rem [filter] \n\n" + 
-                "filter-clean \n\n" + 
-                "wishlist\n\n";
-            await context.PostAsync(reply);
-        }
-        //ADD PRODUCT TO DB (TESTING)
-        else if(details[0].Equals("add", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new AddProductDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //FILTER PRODUCT
-        else if (details[0].Equals("filter", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new FilterDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //FILTER REMOVE
-        else if (details[0].Equals("filter-rem", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new FilterDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //FILTER CLEAN
-        else if (details[0].Equals("filter-clean", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new FilterDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //PRODUCT DETAILS
-        else if(details[0].Equals(BotDefaultAnswers.show_product_details, StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new ProductDetails(), this.StartAsync, message, CancellationToken.None);
-        }
-        //VIEW WISH LIST
-        else if (details[0].Equals("wishlist", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new WishListDialog(WishListDialog.Action.VIEW), this.StartAsync, message, CancellationToken.None);
-        }
-        //ADD PRODUCT TO WISH LIST
-        else if (details[0].Equals(BotDefaultAnswers.add_wish_list, StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new WishListDialog(WishListDialog.Action.ADD), this.StartAsync,message, CancellationToken.None);
-        }
-        //REM PRODUCT FROM WISH LIST
-        else if (details[0].Equals(BotDefaultAnswers.rem_wish_list, StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new WishListDialog(WishListDialog.Action.REM), this.StartAsync, message, CancellationToken.None);
-        }
-        //ADD PRODUCT TO COMPARATOR
-        else if (details[0].Equals(BotDefaultAnswers.add_to_comparator, StringComparison.CurrentCultureIgnoreCase))
-        {
-            //TODO
-        }
-        //ADD CUSTOMER CARD
-        else if (details[0].Equals("set-customer-card", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //ADD CUSTOMER EMAIL
-        else if (details[0].Equals("set-customer-email", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //IDENTIFICATION BY CUSTOMER CARD
-        else if (details[0].Equals("customer-card", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //IDENTIFICATION BY EMAIL
-        else if (details[0].Equals("customer-email", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //FIRST DIALOG
-        else if (details[0].Equals("first-dialog", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //CUSTOMER INFO
-        else if (details[0].Equals("customer-info", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //SET CUSTOMER NAME
-        else if (details[0].Equals("set-customer-name", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //SET CUSTOMER COUNTRY
-        else if (details[0].Equals("set-customer-country", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //ADD CUSTOMER CARD CONFIRMATION
-        else if (details[0].Equals("SaveCard", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //ADD CUSTOMER EMAIL CONFIRMATION
-        else if (details[0].Equals("SaveEmail", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //ADD CHANNEL CONFIRMATION
-        else if (details[0].Equals("AddChannel", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //ADD CUSTOMER NAME 
-        else if (details[0].Equals("SaveName", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        //ADD CUSTOMER COUNTRY 
-        else if (details[0].Equals("SaveCountry", StringComparison.CurrentCultureIgnoreCase))
-        {
-            await context.Forward(new IdentificationDialog(), this.StartAsync, message, CancellationToken.None);
-        }
-        else
-        {
-            var reply = context.MakeMessage();
-            */
     }
 }
